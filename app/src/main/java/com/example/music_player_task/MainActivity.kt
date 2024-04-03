25b1eb0d5f26d735d8c73822003765a7ea514572
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-04-04 00:08:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/music_player_task/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.music_player_task
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.music_player_task.ui.theme.Music_Player_TaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Music_Player_TaskTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Music_Player_TaskTheme {
+        Greeting("Android")
+    }
+}
